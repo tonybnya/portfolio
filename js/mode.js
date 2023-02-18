e3259dc6c415953fddbff348ec0f9922f35a4665
Tony <nya.tony2010@gmail.com>
2023-02-18 08:54:44 +0100

    update

---
+++ js/mode.js
@@ -0,0 +1,32 @@
+const log = (arg) => console.log(arg);
+
+const mode = document.querySelector(".icon-mode");
+
+const light = 'light_mode';
+const bgcl = '#F2F4F7';
+const tcl = '#001F3F';
+
+const dark = 'dark_mode';
+const bgcd = '#223C50';
+const tcd = '#FFFFFF';
+
+const body = document.body;
+
+// Set initial state
+body.style.backgroundColor = bgcl;
+body.style.color = tcl;
+mode.innerText = dark;
+
+mode.addEventListener('click', function() {
+    if (mode.innerText === light) {
+        body.style.backgroundColor = bgcl;
+        body.style.color = tcl;
+        mode.innerText = dark;
+    } else {
+        body.style.backgroundColor = bgcd;
+        body.style.color = tcd;
+        mode.innerText = light;
+    }
+});
+
+log(mode);
