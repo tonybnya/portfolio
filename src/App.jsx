de6ce6af687b3560808a72d1967b644bdc83f92e
Tony <nya.tony2010@gmail.com>
2024-03-21 16:58:14 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">
+          Hello, World!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        righteous: ["Righteous", "sans-serif"],
+        source: ["Source Sans 3", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
+
