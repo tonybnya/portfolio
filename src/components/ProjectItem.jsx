3cf608643ce5c82531540b6ec1b343863a1d387f
Tony <nya.tony2010@gmail.com>
2024-03-22 10:36:42 +0100

    feat: add Projects component

---
+++ src/components/ProjectItem.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function ProjectItem({ imgUrl, title, description, stack, link, source }) {
+  return (
+      <div className="border-2 border-stone-900 rounded-md overflow-hidden">
+          <img
+            src={imgUrl}
+            alt="portfolio" 
+            className="w-full h-36 md:h-48 object-cover"
+         />
+         <div className="w-full p-4">
+            <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-righteous">{title}</h3>
+            <p className='text-justify mb-2 font-light'>{description}</p>
+            <p className="flex flex-wrap gap-2 flex-row items-center justify-start mb-4 text-xs md:text-sm">
+               {stack.map(item => (
+                  <span className="inline-block px-2 py-1 font-semibold border-2 border-stone-900 rounded-md">
+                     {item}
+                  </span>
+               ))}
+            </p>
+              <div className='flex gap-3'>
+                  <a href={link} target='_blank'><i class="fa-solid fa-globe"></i> Live</a>
+                  <a href={source} target='_blank'><i class="fa-brands fa-github"></i> Source</a>
+            </div>
+         </div>
+    </div>
+  )
+}
+
+export default ProjectItem
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Title from './Title'
+import projects from '../data/projects'
+import ProjectItem from './ProjectItem'
 
 function Projects() {
   return (
-    <div>
-      <Title>Projects</Title>
+    <div className="flex flex-col md:flex-row items-center justify-center mb-12">
+      {/* <Title>Projects</Title> */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {projects.map(project => (
+            <ProjectItem 
+              imgUrl={project.imgUrl}
+              title={project.title}
+              description={project.description}
+              stack={project.stack}
+              link={project.link}
+              source={project.source}
+            />
+        ))}
+      </div>
     </div>
   )
 }
