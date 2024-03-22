42d40d18da8ccf9ed1efb053e1ba3dc356dd3ba5
Tony <nya.tony2010@gmail.com>
2024-03-22 09:23:44 +0100

    style: center Title component of each section

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,7 +4,7 @@ function Title({children, id}) {
   return (
       <h1
           id={id && id}
-          className="text-2xl font-righteous underline decoration-4 mb-4">
+          className="text-3xl font-righteous underline decoration-4 mb-4 max-sm:text-xl text-center">
           {children}
     </h1>
   )
