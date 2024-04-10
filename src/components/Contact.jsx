740feae274029f4dccaf10e6dcb51720f9817d03
Tony <nya.tony2010@gmail.com>
2024-04-10 16:06:50 +0100

    style: center the button horizontally

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -31,7 +31,7 @@ function Contact() {
           />
           <button
             type="button"
-            className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md font-righteous text-white bg-gradient-to-r from-blue-500 to-pink-500 drop-shadow-md"
+            className="text-center mr-auto ml-auto inline-block px-8 py-3 w-max text-base font-medium rounded-md font-righteous text-white bg-gradient-to-r from-blue-500 to-pink-500 drop-shadow-md"
           >
             Work With Me
           </button>
