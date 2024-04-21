7b58def8f621d097dfa76115c41a89249de98886
Tony <nya.tony2010@gmail.com>
2024-04-21 16:11:31 +0100

    update portfolio/projects section

--- src/data/projects.js
+++ src/data/projects.js
@@ -9,22 +9,31 @@ const projects = [
     source: "https://github.com/tonybnya/portfolio",
   },
   {
-    imgUrl: "/assets/2.png",
-    title: "Terminal Website",
+    imgUrl: "/assets/1.png",
+    title: "Developer Portfolio",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    stack: ["HTML", "CSS", "JavaScript"],
-    link: "https://fkcodes.com",
-    source: "https://fkcodes.com",
+      "Building a sleek portfolio website with React, Tailwind, and Vite to showcase my skills and projects elegantly, and elevate my online presence with modern technology stack. Seamlessly navigate through responsive design.",
+    stack: ["React", "Tailwind", "Vite"],
+    link: "https://tonybnya.github.io/portfolio/",
+    source: "https://github.com/tonybnya/portfolio",
   },
   {
-    imgUrl: "/assets/3.png",
-    title: "VacAI",
+    imgUrl: "/assets/1.png",
+    title: "Developer Portfolio",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    stack: ["Python", "C++", "Robotics"],
-    link: "https://youtu.be/A8eegxL82PI",
-    source: "https://youtu.be/A8eegxL82PI",
+      "Building a sleek portfolio website with React, Tailwind, and Vite to showcase my skills and projects elegantly, and elevate my online presence with modern technology stack. Seamlessly navigate through responsive design.",
+    stack: ["React", "Tailwind", "Vite"],
+    link: "https://tonybnya.github.io/portfolio/",
+    source: "https://github.com/tonybnya/portfolio",
+  },
+  {
+    imgUrl: "/assets/1.png",
+    title: "Developer Portfolio",
+    description:
+      "Building a sleek portfolio website with React, Tailwind, and Vite to showcase my skills and projects elegantly, and elevate my online presence with modern technology stack. Seamlessly navigate through responsive design.",
+    stack: ["React", "Tailwind", "Vite"],
+    link: "https://tonybnya.github.io/portfolio/",
+    source: "https://github.com/tonybnya/portfolio",
   },
 ];
 
