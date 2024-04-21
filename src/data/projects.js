0f5119c8af0efe561a675f749f1631d4626f8913
Tony <nya.tony2010@gmail.com>
2024-04-21 17:36:47 +0100

    build: prepare the app for deployment

--- src/data/projects.js
+++ src/data/projects.js
@@ -26,15 +26,6 @@ const projects = [
     link: "https://tonybnya.github.io/portfolio/",
     source: "https://github.com/tonybnya/portfolio",
   },
-  {
-    imgUrl: "/assets/1.png",
-    title: "Developer Portfolio",
-    description:
-      "Building a sleek portfolio website with React, Tailwind, and Vite to showcase my skills and projects elegantly, and elevate my online presence with modern technology stack. Seamlessly navigate through responsive design.",
-    stack: ["React", "Tailwind", "Vite"],
-    link: "https://tonybnya.github.io/portfolio/",
-    source: "https://github.com/tonybnya/portfolio",
-  },
 ];
 
 export default projects;
