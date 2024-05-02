342b3512629176dd3289c8999b1059005b12e476
tonybnya <nya.tony2010@gmail.com>
2024-05-02 21:26:05 +0100

    add KaKo project details

--- src/data/projects.js
+++ src/data/projects.js
@@ -9,13 +9,13 @@ const projects = [
     source: "https://github.com/tonybnya/portfolio",
   },
   {
-    imgUrl: "/assets/1.png",
-    title: "Developer Portfolio",
+    imgUrl: "/assets/2.png",
+    title: "KaKo",
     description:
-      "Building a sleek portfolio website with React, Tailwind, and Vite to showcase my skills and projects elegantly, and elevate my online presence with modern technology stack. Seamlessly navigate through responsive design.",
-    stack: ["React", "Tailwind", "Vite"],
+      "Embark on a thrilling journey through the Batman universe with KaKo, an innovative e-commerce platform that brings the essence of Gotham City to your fingertips. Immerse yourself in a world where the iconic meets the extraordinary, as you explore a curated collection of merchandise inspired by the legendary Dark Knight. Built with React, Tailwind CSS, Vite, Flask, and MySQL, KaKo combines cutting-edge technology with the timeless allure of Batman, delivering a seamless and captivating shopping experience.",
+    stack: ["React", "Tailwind", "Vite", "Flask", "MySQL"],
     link: "https://tonybnya.github.io/portfolio/",
-    source: "https://github.com/tonybnya/portfolio",
+    source: "https://github.com/tonybnya/kako",
   },
   {
     imgUrl: "/assets/1.png",
