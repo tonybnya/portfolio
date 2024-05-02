c453ef7186936c7db29f7ff13cd97928c46f8907
tonybnya <nya.tony2010@gmail.com>
2024-05-02 21:28:12 +0100

    change the description of KaKo project

--- src/data/projects.js
+++ src/data/projects.js
@@ -12,7 +12,7 @@ const projects = [
     imgUrl: "/assets/2.png",
     title: "KaKo",
     description:
-      "Embark on a thrilling journey through the Batman universe with KaKo, an innovative e-commerce platform that brings the essence of Gotham City to your fingertips. Immerse yourself in a world where the iconic meets the extraordinary, as you explore a curated collection of merchandise inspired by the legendary Dark Knight. Built with React, Tailwind CSS, Vite, Flask, and MySQL, KaKo combines cutting-edge technology with the timeless allure of Batman, delivering a seamless and captivating shopping experience.",
+      "Embark on a thrilling journey through the Batman universe with KaKo, an innovative e-commerce platform that brings the essence of Gotham City to your fingertips. Immerse yourself in a world where the iconic meets the extraordinary.",
     stack: ["React", "Tailwind", "Vite", "Flask", "MySQL"],
     link: "https://tonybnya.github.io/portfolio/",
     source: "https://github.com/tonybnya/kako",
