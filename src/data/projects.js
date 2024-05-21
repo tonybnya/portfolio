3c7ff99f75f9e22365e9285f8be5801a056eb065
tonybnya <nya.tony2010@gmail.com>
2024-05-21 14:25:12 +0100

    add some projects data

--- src/data/projects.js
+++ src/data/projects.js
@@ -9,6 +9,15 @@ const projects = [
     source: "https://github.com/tonybnya/portfolio",
   },
   {
+    imgUrl: "/assets/3.png",
+    title: "Portfolio API",
+    description:
+      "Building a CRUD API with Python, Django, and Django REST to serve my Portfolio website with projects, and timelines (milestones or progression or steps as a Software Engineer) progressively.",
+    stack: ["Python", "Django", "Django REST"],
+    link: "https://github.com/tonybnya/portfolio-api",
+    source: "https://github.com/tonybnya/portfolio-api",
+  },
+  {
     imgUrl: "/assets/2.png",
     title: "KaKo",
     description:
@@ -16,15 +25,6 @@ const projects = [
     stack: ["React", "Tailwind", "Vite", "Flask", "MySQL"],
     link: "https://kako-landing-page.onrender.com/",
     source: "https://github.com/tonybnya/kako",
-  },
-  {
-    imgUrl: "/assets/1.png",
-    title: "Developer Portfolio",
-    description:
-      "Building a sleek portfolio website with React, Tailwind, and Vite to showcase my skills and projects elegantly, and elevate my online presence with modern technology stack. Seamlessly navigate through responsive design.",
-    stack: ["React", "Tailwind", "Vite"],
-    link: "https://tonybnya-portfolio.onrender.com/",
-    source: "https://github.com/tonybnya/portfolio",
   },
 ];
 
