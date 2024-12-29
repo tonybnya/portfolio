e7d8db6c94d03aa7f4af1a21d1ea9a2c2c003031
tonybnya <nya.tony2010@gmail.com>
2024-12-29 19:28:25 +0100

    style: add some improvements to emphase some parts of the intro text

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -20,40 +20,43 @@ function Intro() {
       </h3>
       <p className="dark:text-zinc-50 text-black text-justify py-4 font-light text-sm tracking-wide max-sm:text-xs">
         Dedicated and adaptable professional, I&apos;m a Software Engineer
-        Graduated in ✨ 12-month Software Engineering Programs and ✨ Artificial
-        Intelligence Career Essentials (AiCE) by{" "}
+        Graduated in ✨ <span className="underline font-bold text-gray-900 dark:text-white decoration-pink-500 decoration-2">12-month Software Engineering</span> Programs and ✨ <span className="underline font-bold text-gray-900 dark:text-white decoration-pink-500 decoration-2">Artificial
+        Intelligence Career Essentials</span> (AiCE) by{" "}
         <a
           target="_blank"
-          className="underline"
+          className="underline font-semibold text-gray-900 dark:text-white decoration-indigo-500"
           href="https://www.alxafrica.com"
         >
           ALX
         </a>
-        , in ✨ 6-month Web Development by{" "}
-        <a target="_blank" className="underline" href="https://sayna.io">
+        , in ✨ <span className="underline font-bold text-gray-900 dark:text-white decoration-pink-500 decoration-2">
+        6-month Web Development</span> by{" "}
+        <a target="_blank" className="underline font-semibold text-gray-900 dark:text-white decoration-indigo-500" href="https://sayna.io">
           Sayna Academy
         </a>
         , and ✨ the inaugural cohort of the{" "}
         <a
           target="_blank"
-          className="underline"
+          className="underline font-semibold text-gray-900 dark:text-white decoration-indigo-500"
           href="https://github.blog/2023-10-09-skilling-african-developers-through-all-in-africa/"
         >
           All In Open Source
         </a>{" "}
         Program by{" "}
-        <a target="_blank" className="underline" href="https://github.com">
+        <a target="_blank" className="underline font-semibold text-gray-900 dark:text-white decoration-indigo-500" href="https://github.com">
           GitHub
         </a>
-        . I&apos;m also holding a ✨ Bachelor&apos;s Degree in Computer Science
-        and almost a ✨ decade of experience/background in Diplomacy and Public
-        Administration. Skilled in &bull; Project Management &bull; Writing
+        . I&apos;m also holding a ✨ <span className="underline font-bold text-gray-900 dark:text-white decoration-pink-500 decoration-2">
+        Bachelor&apos;s Degree in Computer Science</span>{" "}
+        and almost a ✨ <span className="underline font-bold text-gray-900 dark:text-white decoration-pink-500 decoration-2">
+        decade of experience/background in Diplomacy and Public
+        Administration</span>. Skilled in &bull; Project Management &bull; Writing
         &bull; Problem-Solving &bull; Essential AI Career Tools, with a &bull;
-        strong foundation in Software Development and a &bull; Soft Skills
-        Certified by{" "}
+        strong foundation in Software Development and a &bull; <span className="underline font-bold text-gray-900 dark:text-white decoration-pink-500 decoration-2">
+        Soft Skills Certified</span> by{" "}
         <a
           target="_blank"
-          className="underline"
+          className="underline font-semibold text-gray-900 dark:text-white decoration-indigo-500"
           href="https://www.helloelton.com"
         >
           Hello Elton
