594a2b9939a59e56213487d2f941a05bd6aa5297
tonybnya <nya.tony2010@gmail.com>
2024-12-29 19:37:45 +0100

    style: add transition on hover to the Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,10 +4,11 @@ const Button = ({ type, text, icon }) => {
   return (
     <button
       type={type}
-      className="flex items-center text-center gap-3 mr-auto ml-auto px-8 py-3 w-max text-base font-medium rounded-md font-righteous text-white bg-gradient-to-r from-blue-500 to-pink-500 drop-shadow-md"
+      className="group flex items-center text-center gap-3 mr-auto ml-auto px-8 py-3 w-max text-base font-medium rounded-md font-righteous text-white bg-gradient-to-r from-blue-500 to-pink-500 drop-shadow-md"
     >
       {text}
       {icon}
+      <div className="absolute inset-0 h-full w-full scale-0 rounded-md transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30"></div>
     </button>
   );
 };
