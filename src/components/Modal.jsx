efa8bfb38436b485c02d700f3585e9b5bee406af
tonybnya <nya.tony2010@gmail.com>
2024-12-30 18:38:07 +0100

    style: change the background of the modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,7 +4,7 @@ const Modal = ({ title, description }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="flex">
+    <div className="">
       <button
         onClick={() => setIsOpen(true)}
         className="group flex justify-start items-center text-center tracking-wide gap-3 mr-auto ml-auto px-8 py-1 w-max text-base font-medium rounded-md font-righteous text-white bg-gradient-to-r from-[#007A5E] via-[#CE1126] to-[#FCD116] drop-shadow-md"
@@ -20,14 +20,14 @@ const Modal = ({ title, description }) => {
         >
           <div
             onClick={() => setIsOpen(false)}
-            className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+            className="absolute inset-0 bg-stone-900 bg-opacity-75 transition-opacity"
           ></div>
 
           <div
             className="bg-white rounded-md shadow-xl overflow-hidden max-w-md w-full sm:w-96 md:w-1/2 lg:w-2/3 xl:w-1/3 z-50 transform transition-transform duration-300"
           >
             {/* <div className="bg-gradient-to-r from-black via-blue-500 to-red-900 text-white px-4 py-2 flex justify-between"> */}
-            <div className="bg-gradient-to-r from-[#007A5E] via-[#CE1126] to-[#FCD116] text-white px-4 py-2 flex justify-between">
+            <div className="bg-gradient-to-r from-[#007A5E] via-[#CE1126] to-[#FCD116] text-white px-4 py-2 flex justify-center">
               <h2 className="text-lg font-bold font-righteous tracking-wider">{title}</h2>
             </div>
 
