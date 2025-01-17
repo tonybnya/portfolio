c60888bf8f3ac6e0120c7ce8cc77f0f30090ed3a
tonybnya <nya.tony2010@gmail.com>
2025-01-17 19:49:28 +0100

    style: remove the drop shadow on the Skill component

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Skill = ({ svg, title }) => {
   return (
-    <div className="flex flex-col justify-center items-center max-w-sm p-6 m-6 drop-shadow-blue dark:drop-shadow-custom bg-dark border border-gray-200 rounded-lg shadow hover:bg-gray-500 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <div className="flex flex-col justify-center items-center max-w-sm p-6 m-6 rounded-lg shadow hover:bg-gray-500 bg-black dark:bg-gray-800 dark:hover:bg-gray-700">
       <div className="w-20 h-20 p-2 flex justify-center items-center">
         {svg}
       </div>
