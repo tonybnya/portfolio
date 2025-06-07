b57297966e86d59b034b71d9c8616c99e58dc67c
tonybnya <nya.tony2010@gmail.com>
2025-06-07 21:18:23 +0100

    fix: update button component to handle click events

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,9 @@
-const Button = ({ type, text, icon }) => {
+const Button = ({ type, text, icon, onClick, disabled }) => {
   return (
     <button
       type={type}
+      onClick={onClick}
+      disabled={disabled}
       className="group flex items-center text-center tracking-wider gap-3 mr-auto ml-auto px-8 py-3 w-max text-base font-medium rounded-md font-righteous text-white bg-black hover:bg-black/50"
     >
       {text}
