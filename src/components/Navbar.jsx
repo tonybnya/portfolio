163e03ee804d1ea34c4e07851b811bca74f4ad24
tonybnya <nya.tony2010@gmail.com>
2025-06-24 19:37:46 +0100

    style: add white shadow to the hamburger menu icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
         >
           <span className="sr-only">Open main menu</span>
           <svg
-            className="w-5 h-5 text-white"
+            className="w-5 h-5 text-white shadow-lg shadow-white/50 drop-shadow-xl"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
