5173ab0ceff20a862a81df47baffde156c1bba8a
tonybnya <tonybnya@gmail.com>
2026-08-11 00:32:23 +0100

    style(title): switch section title text to white

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ const Title = ({ children, id }) => {
     <div className="flex justify-center items-center mt-20 mx-auto p-4 bg-[#050403] w-[200px] rounded-3xl shadow-lg shadow-white/50 drop-shadow-xl">
       <h1
         id={id && id}
-        className="text-2xl font-sans font-medium text-[#1d1d1d] text-center tracking-normal"
+        className="text-2xl font-sans font-medium text-white text-center tracking-normal"
       >
         {children}
       </h1>
